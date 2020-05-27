259e74ba53a3ab237741d977e5de5e11bff0a2ca
하성준 <haseongjun@haseongjun-ui-MacBookPro.local>
2020-05-27 16:36:42 +0900

    add README.md

---
+++ src/main/java/com/ronnie/first/demo/LoadDatabase.java
@@ -0,0 +1,20 @@
+package com.ronnie.first.demo;
+
+import lombok.extern.slf4j.Slf4j;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@Slf4j
+class LoadDatabase {
+
+    @Bean
+    CommandLineRunner initDatabase(UserRepository repository) {
+        return args -> {
+            log.info("Preloading " + repository.save(new User("Ronnie", "manager")));
+            log.info("Preloading " + repository.save(new User("Alex", "superUser")));
+        };
+    }
+}
